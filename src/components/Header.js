18457467c56de2c65ca467fe2ae765c914b42a8d
adominguez <dmartin.alberto@gmail.com>
2021-04-29 22:15:52 +0200

    feat(logo-movil, header): add logo movil and favicon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import logo from '../img/logo.svg'
+import logoMovil from '../img/logo-movil.svg'
 import NavButton from './NavButton'
 import SearchInput from './SearchInput'
 
@@ -9,16 +10,16 @@ const Navbar = ({ focusSearch, cancelSearch, searchOpened, menuOpened, toggleMen
   return (
     <header className={`bg-white principal-container flex justify-center w-full ${menuOpened ? 'z-20' : ''} ${isScrolling ? 'shadow-lg' : ''}`}>
       <div className={`justify-between w-full flex md:p-4 relative`}>
-        <Link to="/" className={`leading-none text-2xl sm:block sm:text-center no-underline outline-none text-white hover:text-secondary-500 focus:text-secondary-500 w-60 p-2 md:p-0`}>
+        <Link to="/" className={`leading-none text-2xl sm:block sm:text-center no-underline outline-none text-white hover:text-secondary-500 focus:text-secondary-500 w-16 md:w-60 p-2 md:p-0`}>
           <picture className="w-30">
-            <source media="(max-width: 768px)" srcSet={logo}></source>
+            <source media="(max-width: 768px)" srcSet={logoMovil}></source>
             <img className="md:w-full md:h-full" loading="lazy" src={logo} alt="3DMakerNow" />
           </picture>
         </Link>
         <div className="relative flex items-center justify-center flex-1 mx-2 md:mx-3">
           <SearchInput onfocusInput={() => focusSearch()} onCancelSearch={() => cancelSearch()} opened={searchOpened} />
         </div>
-        <nav role="navigation" aria-label="main-navigation" className={`flex absolute flex-col w-full z-20 bg-white top-16 ${menuOpened ? 'overflow-auto h-auto' : 'overflow-hidden h-0'} md:h-auto md:relative md:top-0 md:flex-row md:w-auto md:z-0`}>
+        <nav role="navigation" aria-label="main-navigation" className={`flex absolute flex-col w-full z-20 bg-white top-16 ${menuOpened ? 'overflow-auto h-auto' : 'overflow-hidden h-0'} md:h-auto md:relative md:top-0 md:flex-row md:w-auto md:z-0 items-center`}>
           <Link className="p-2" to="/about">
             About
           </Link>
