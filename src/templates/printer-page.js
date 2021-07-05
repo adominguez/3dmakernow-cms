66ac5fe60f118c58995c15cefc9374e8f8c7cdfc
adominguez <dmartin.alberto@gmail.com>
2021-07-05 23:25:29 +0200

    fix(printer-page): remove Submenu import

--- src/templates/printer-page.js
+++ src/templates/printer-page.js
@@ -4,7 +4,6 @@ import { Helmet } from 'react-helmet'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
 import Content, { HTMLContent } from '../components/Content'
-import Submenu from '../components/Submenu'
 
 export const PrinterPageTemplate = (props) => {
   const {
@@ -17,7 +16,6 @@ export const PrinterPageTemplate = (props) => {
   
   return (
     <>
-      <Submenu title={title} />
       <section className="flex justify-center w-full">
         {helmet || ''}
         <PostContent className="w-full" content={content} />
